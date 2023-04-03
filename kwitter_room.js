83d98d13c3a4479d535d561614029ab546756f05
pup4 <108271433+pup4@users.noreply.github.com>
2023-04-03 16:00:25 +1000

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,5 +1,15 @@
-
-//ADD YOUR FIREBASE LINKS HERE
+var firebaseConfig = {
+      apiKey: "AIzaSyCFeuGSj2C5Bzlv4SoW84S5Eu0UPWwa5us",
+      authDomain: "kwitter-eca32.firebaseapp.com",
+      databaseURL: "https://kwitter-eca32-default-rtdb.firebaseio.com",
+      projectId: "kwitter-eca32",
+      storageBucket: "kwitter-eca32.appspot.com",
+      messagingSenderId: "881595320738",
+      appId: "1:881595320738:web:f378b12fb9f7ee2fe40afb"
+    };
+    
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
 
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
        Room_names = childKey;
@@ -8,3 +18,4 @@ function getData() {firebase.database().ref("/").on('value', function(snapshot) 
       //End code
       });});}
 getData();
+
